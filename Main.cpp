02df4288189a487900e879a25ddabf274eb3e03b
TheGreatestGitUser <joshua123033@gmail.com>
2022-01-31 11:30:03 -0500

    Update Main.cpp - Redefined inputs
    Input statements have been changed to allow for multiple spaces between tokens. Testing was also done to show that #define methods would be best for fast input.

--- Main.cpp
+++ Main.cpp
@@ -25,16 +25,19 @@
 #include <float.h>
 #include <numeric>
 using namespace std;
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0) // This one isn't needed
-//#define nextInteger(x) do{ bool neg = false; register int c; x = 0; c = getchar_unlocked(); if (c=='-') { neg = true; c = getchar_unlocked(); } for (; (c>47 && c<58); c=getchar_unlocked()) { x = x * 10 + c - 48; } if (neg){ x *= -1; } } while (0) // This one is used for integers
-#define nextInteger(x) do{ bool neg = false; register int c; x = 0; c = get(); if (c=='-') { neg = true; c = get(); } for (; (c>47 && c<58); c=get()) { x = x * 10 + c - 48; } if (neg){ x *= -1; } } while (0) // This one is used for integers
-//#define nextDecimal(x) do{ double div = 1; bool neg = false; register int c = getchar_unlocked(); x = 0; while (c < 33) c = getchar_unlocked(); if (c=='-') { neg = true; c = getchar_unlocked(); } for (; (c>47 && c<58); c=getchar_unlocked()) { x = x * 10 + c - 48; } if (c=='.') { c=getchar_unlocked(); for (; (c>47 && c<58); c=getchar_unlocked()) { x += (c - '0') / (div *= 10); } } if (neg){ x *= -1; } } while (0) // This one is used for decimals, but doesn't work, so comment out later.
-#define nextDecimal(x) do{ double div = 1; bool neg = false; register int c = get(); x = 0; while (c < 33) c = get(); if (c=='-') { neg = true; c = get(); } for (; (c>47 && c<58); c=get()) { x = x * 10 + c - 48; } if (c=='.')
-//#define next(x) do{ register char c = getchar_unlocked(); while (c < 33){ c = getchar_unlocked(); } os.str(""); while (c > 32){ os << c; c = getchar_unlocked(); } x = os.str(); } while (0) // This one is used for the next token in stdin
-#define next(x) cin >> x;
-//#define nextLine(x) do{ register char c = getchar_unlocked(); while (c < 11){ c = getchar_unlocked(); } os.str(""); while (c > 10){ os << c; c = getchar_unlocked(); } x = os.str(); } while (0) // This one is used for the next line in stdin
-#define nextLine(x) getline(cin, x);
+//#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0) // This one isn't needed
+#define nextInteger(x) do{ bool neg = false; register int c; x = 0; while (c < 33) c = getchar_unlocked(); if (c=='-') { neg = true; c = getchar_unlocked(); } for (; (c>47 && c<58); c=getchar_unlocked()) { x = x * 10 + c - 48; } if (neg){ x *= -1; } } while (0) // This one is used for integers
+//#define nextInteger(x) do{ bool neg = false; register int c; x = 0; c = get(); if (c=='-') { neg = true; c = get(); } for (; (c>47 && c<58); c=get()) { x = x * 10 + c - 48; } if (neg){ x *= -1; } } while (0) // This one is used for integers
+#define nextDecimal(x) do{ double div = 1; bool neg = false; register int c = getchar_unlocked(); x = 0; while (c < 33) c = getchar_unlocked(); if (c=='-') { neg = true; c = getchar_unlocked(); } for (; (c>47 && c<58); c=getchar_unlocked()) { x = x * 10 + c - 48; } if (c=='.') { c=getchar_unlocked(); for (; (c>47 && c<58); c=getchar_unlocked()) { x += (c - '0') / (div *= 10); } } if (neg){ x *= -1; } } while (0) // This one is used for decimals, but doesn't work, so comment out later.
+//#define nextDecimal(x) do{ double div = 1; bool neg = false; register int c = get(); x = 0; while (c < 33) c = get(); if (c=='-') { neg = true; c = get(); } for (; (c>47 && c<58); c=get()) { x = x * 10 + c - 48; } if (c=='.')
+#define next(x) do{ register char c = getchar_unlocked(); while (c < 33) c = getchar_unlocked(); os.str(""); while (c > 32){ os << c; c = getchar_unlocked(); } x = os.str(); } while (0) // This one is used for the next token in stdin
+//#define next(x) cin >> x;
+#define nextLine(x) do{ register char c = getchar_unlocked(); while (c < 11) c = getchar_unlocked(); os.str(""); while (c > 10){ os << c; c = getchar_unlocked(); } x = os.str(); } while (0) // This one is used for the next line in stdin
+//#define nextLine(x) getline(cin, x);
 //#define nextDecimal(x) do{ string s; next(s); x = stold(s); } while (0)
+//https://stackoverflow.com/questions/3203452/how-to-read-entire-stream-into-a-stdstring
+//^this link can be used if you want to read the entire stream into a string.
+
 char _;
 //https://stackoverflow.com/questions/1637332/static-const-vs-define
 #define INF 0x3f3f3f3f
